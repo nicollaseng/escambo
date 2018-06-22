206e0986ad43bc37043924be2957ba386260a556
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-06-22 14:19:19 -0300

    Creating first models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	puts "Cadastrando Categorias"
+ categories = ["Acessórios para Veículos",
+ "Agro, Indústria e Comércio",
+ "Alimentos e Bebidas",
+ "Animais ",
+ "Antiguidades",
+ "Arte e Artesanato", 
+ "Bebês",
+ "Beleza e Cuidado Pessoal ",
+ "Brinquedos e Hobbies", 
+ "Câmeras e Acessórios",
+ "Calçados, Roupas e Bolsas",
+ "Casa, Móveis e Decoração",
+ "Celulares e Telefones", 
+ "Eletrodomésticos", 
+ "Eletrônicos, Áudio e Vídeo",
+ "Esportes e Fitness", 
+ "Games ",
+ "Informática"] 
+
+ categories.each do |category|
+ 	Category.find_or_create_by!(description: category)
+ end
+
+ puts "Categorias cadastradas com sucesso"

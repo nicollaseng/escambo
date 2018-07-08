5bf6c2a51c6a641216d655b8dbf4615191efdcf6
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-07-08 20:01:28 -0300

    Admin views changed

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     get '/home', to: 'home#index'
   end
 
-  devise_for :admins, :skip => [:registrations] 
+  devise_for :admins
   devise_for :members
   
   root 'site/home#index'

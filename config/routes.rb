6046e3dd7001a308c1516a4203cd81a6c8877c46
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-07-08 21:43:24 -0300

    View for admin creation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
 
   namespace :backoffice do
+    resources :admins
     resources :category
     get 'categories/index'
     get 'dashboard', to: 'dashboard#index'
@@ -21,5 +22,7 @@ Rails.application.routes.draw do
   devise_for :members
   
   root 'site/home#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

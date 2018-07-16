255a4812d8aa8378ec5f6c45206f6d17ae8bcc8f
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-16 12:18:43 -0300

    update currency to brl

--- db/migrate/20180716150204_add_price_cents_to_ads.rb
+++ db/migrate/20180716150204_add_price_cents_to_ads.rb
@@ -1,5 +1,5 @@
 class AddPriceCentsToAds < ActiveRecord::Migration[5.2]
   def change
-    add_column :ads, :price_cents, :integer
+    add_column :ads, :price_cents, :integer, default: 0
   end
 end
